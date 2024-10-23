fc99a5c45945e10079828f1b65e6d4363c3be7ac
EmanuelFNG <manuelgody@hotmail.com>
2024-10-23 20:50:27 -0300

    Etapa - 3

---
+++ Etapa - 3/index.js
@@ -0,0 +1,8 @@
+
+window.alert('Seja Bem-Vindo!') // caixa de alerta (dialogo)
+window.prompt('Qual é o seu nome?') // vai perguntar o nome.
+
+
+var Materias = prompt("Informe suas novas UCs")
+document.write("Uc incluida, ", Nome)
+console.log("Informe suas UCs")
